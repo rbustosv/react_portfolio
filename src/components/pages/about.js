f635a661f8488f44837103c2c849def86cde5d2e
Raisa Bustos <raisa.bustosv@gmail.com>
2019-07-25 23:45:22 -0600

    Updating paragraph content page

--- src/components/pages/about.js
+++ src/components/pages/about.js
@@ -15,9 +15,8 @@ export default function() {
       <div className="right-column">
       <p>I am a systems engineer with experience in different areas of IT, such as infrastructure, support, and software development which have increased my professional knowledge and skills. 
       I have a passion for web development, especially HTML, CSS, JQuery, JavaScript, Python.
-      I consider challenges as essential elements for personal growth. 
-      Ingeniera de sistemas con mención en telemática.</p>
-      <p>
+      I consider challenges as essential elements for personal growth.</p>
+      <p>Ingeniera de sistemas con mención en telemática.
       Mi experiencia laboral en diversas áreas de TI, como infraestructura, soporte y desarrollo me ha permitido ampliar mis conocimientos y destrezas en el ámbito profesional. 
       Mi fascinación, todo lo relacionado a desarrollo web. HTML, CSS, JavaScript, Python, JQuery.
       Considero a los retos o desafíos elementos esenciales para el desarrollo personal y profesional.
