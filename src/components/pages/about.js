7e135ece6d650b6454c2526e16f26bb314ee1ebf
Raisa Bustos <raisa.bustosv@gmail.com>
2019-07-25 23:38:23 -0600

    Updating about content page

--- src/components/pages/about.js
+++ src/components/pages/about.js
@@ -13,23 +13,15 @@ export default function() {
         marginBottom: "10px"
       }}/>
       <div className="right-column">
-        Maecenas faucibus mollis interdum. Integer posuere erat a ante venenatis
-        dapibus posuere velit aliquet. Sed posuere consectetur est at lobortis.
-        Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui.
-        Aenean lacinia bibendum nulla sed consectetur. Maecenas sed diam eget
-        risus varius blandit sit amet non magna. Morbi leo risus, porta ac
-        consectetur ac, vestibulum at eros. Donec id elit non mi porta gravida
-        at eget metus. Donec sed odio dui. Cras mattis consectetur purus sit
-        amet fermentum. Etiam porta sem malesuada magna mollis euismod. Nulla
-        vitae elit libero, a pharetra augue. Aenean eu leo quam. Pellentesque
-        ornare sem lacinia quam venenatis vestibulum. Duis mollis, est non
-        commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec
-        elit. Praesent commodo cursus magna, vel scelerisque nisl consectetur
-        et. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent
-        commodo cursus magna, vel scelerisque nisl consectetur et. Nullam quis
-        risus eget urna mollis ornare vel eu leo. Morbi leo risus, porta ac
-        consectetur ac, vestibulum at eros. Vestibulum id ligula porta felis
-        euismod semper.
+      <p>I am a systems engineer with experience in different areas of IT, such as infrastructure, support, and software development which have increased my professional knowledge and skills. 
+      I have a passion for web development, especially HTML, CSS, JQuery, JavaScript, Python.
+      I consider challenges as essential elements for personal growth. 
+      Ingeniera de sistemas con mención en telemática.</p>
+      <p>
+      Mi experiencia laboral en diversas áreas de TI, como infraestructura, soporte y desarrollo me ha permitido ampliar mis conocimientos y destrezas en el ámbito profesional. 
+      Mi fascinación, todo lo relacionado a desarrollo web. HTML, CSS, JavaScript, Python, JQuery.
+      Considero a los retos o desafíos elementos esenciales para el desarrollo personal y profesional.
+      </p>
       </div>
     </div>
   );
